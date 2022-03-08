80077c65cd59be6ca8f9b66e7a8539efdae98a2f
Gorea Tudor <tudortudor2002@gmail.com>
2022-03-08 21:35:21 +0200

    added RegisterActivity
    on LogInActivity press NoAccount label to go to RegisterActivity

--- MyTinder_1/app/src/main/java/com/goreatudor/mytinder_1/activities/LogInActivity.java
+++ MyTinder_1/app/src/main/java/com/goreatudor/mytinder_1/activities/LogInActivity.java
@@ -2,7 +2,9 @@ package com.goreatudor.mytinder_1.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.goreatudor.mytinder_1.R;
 
@@ -12,5 +14,16 @@ public class LogInActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
+
+        setup();
+    }
+
+    private void setup() {
+        TextView register = findViewById(R.id.login_label_register);
+
+        register.setOnClickListener(view -> {
+            Intent intent = new Intent(LogInActivity.this, RegisterActivity.class);
+            startActivity(intent);
+        });
     }
 }
