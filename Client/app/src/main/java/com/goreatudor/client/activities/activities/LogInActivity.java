0071bb70ec1d1ef8781ebff69b7fa575f3e2127d
Gorea Tudor <tudortudor2002@gmail.com>
2022-03-14 21:08:27 +0200

    added RegisterActivity

--- Client/app/src/main/java/com/goreatudor/client/activities/activities/LogInActivity.java
+++ Client/app/src/main/java/com/goreatudor/client/activities/activities/LogInActivity.java
@@ -2,15 +2,43 @@ package com.goreatudor.client.activities.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.goreatudor.client.R;
 
+import java.util.Arrays;
+
 public class LogInActivity extends AppCompatActivity {
+
+    EditText txt_mail;
+    EditText txt_pwd;
+    TextView lbl_reg;
+    Button btn_login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
+
+        txt_mail = findViewById(R.id.login_textBox_mail);
+        txt_pwd = findViewById(R.id.login_textBox_pwd);
+        lbl_reg = findViewById(R.id.login_lbl_reg);
+        btn_login = findViewById(R.id.login_btn_login);
+
+        btn_login.setOnClickListener(view -> {
+            String mail = txt_mail.getText().toString();
+            int pwd = Arrays.hashCode(txt_pwd.getText().toString().toCharArray());
+
+            //TODO: send login request to server
+        });
+
+        lbl_reg.setOnClickListener(view -> {
+            Intent intent = new Intent(LogInActivity.this, RegisterActivity.class);
+            startActivity(intent);
+        });
     }
 }
